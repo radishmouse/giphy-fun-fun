8f052b15a6bcb9e4844912ef0d83ef7c556c109f
Chris Aquino <chris@radishmouse.com>
2020-02-06 13:13:31 -0500

    adds code for automatic pulling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GiphyApp from './GiphyApp';
+
+const API_KEY = `H2R4CV5XYRUZEG1jAn5GP4Rl9oWsSeCX`;
+const FREQUENCY = 3000;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GiphyApp apiKey={API_KEY} frequency={FREQUENCY} />
   );
 }
 
